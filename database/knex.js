acebde7971b3dd14e1b948c7061d94bccf488bd3
Leozin89python <leozin069@gmail.com>
2021-09-11 17:11:22 -0300

    bemvindo

--- database/knex.js
+++ database/knex.js
@@ -1,5 +1,6 @@
-const dev  = process.env.NODE_ENV || 'development'
-const db   = require('../knexfile')[dev]
+const dev   = process.env.NODE_ENV     || 'development'
+const prod  = process.env.DATABASE_URL || 'production'
+const db   = require('../knexfile')[prod]
 const knex = require('knex')(db) 
 
 export default knex
