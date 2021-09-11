2722321c30c39fbabca73f2186e7adae5b861895
Leozin89python <leozin069@gmail.com>
2021-09-11 19:14:41 -0300

    bemvindo

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,14 @@
 module.exports = {
 
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: {
+        host:"ec2-3-214-136-47.compute-1.amazonaws.com",
+        database: "dese1p42p8q2f4",
+        user:     "cymtlfyczqpibq",
+        password: "33f5928666a3623a4ecaa9839d662353d3b9390acbdaba3c9b8752129db888f1"
+      }
 
 };
+
+   /* client: 'pg',
+    connection: process.env.DATABASE_URL*/
