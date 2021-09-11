cd271a422a272f3716c84abac60f899ef189f01d
Leozin89python <leozin069@gmail.com>
2021-09-11 19:03:07 -0300

    bemvindo

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use('/hello-heroku',messageRouter)
+
 app.get('/',(req,res)=>{
     return res.status(200).json({msg:"welcome to heroku - app!"})
 })
