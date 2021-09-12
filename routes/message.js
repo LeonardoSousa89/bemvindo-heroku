8fa89b9659886a47b0d38ea3a3fb251aa702f46a
Leozin89python <leozin069@gmail.com>
2021-09-12 01:04:16 -0300

    bemvindo

--- routes/message.js
+++ routes/message.js
@@ -4,7 +4,7 @@ const db      = require('../database/knex')
 const msgRoute = express.Router()
 
 msgRoute.route('/').get(async(req,res)=>{
-        await db.select(['id, msg'])
+        await db.select(['id','msg'])
                 .table('welcome')
                 .then(msg  => res.status(200).json(msg))
                 .catch(err => res.status(500).json(err))
