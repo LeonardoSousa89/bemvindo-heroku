48c794c20eda767c5f1cfd3032d68b166df3669d
Leozin89python <leozin069@gmail.com>
2021-09-12 00:51:33 -0300

    bemvindo

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
         database: "dese1p42p8q2f4",
         user:     "cymtlfyczqpibq",
         password: "33f5928666a3623a4ecaa9839d662353d3b9390acbdaba3c9b8752129db888f1",
-        ssl:      true
+        ssl: { rejectUnauthorized: false }
       }
 
 };
